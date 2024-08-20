925ae39388745825ac550c052b61f790c6b0903a
Joshua Sunday <joshuasunday300@gmail.com>
2024-08-20 12:34:01 +0100

    i commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import WalletConnectionProvider from "./adapter/page";
+import WalletConnectionProvider from "./adapter/page"; // Correct the path to WalletConnectionProvider
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
 import VotingSystem from "./votingsystem/page";
-import CastVote from "./component/castvote/page";
 
 export default function Home() {
   return (
     <WalletConnectionProvider>
-      <VotingSystem />
+      <WalletMultiButton /> {/* Add the wallet connection button */}
+      <VotingSystem /> {/* The main voting system component */}
     </WalletConnectionProvider>
   );
 }
